cac57226bbb4e86a6bf5bad0d367d3dde86070a9
Pasha <qordon23@gmail.com>
2023-02-28 18:11:54 +0400

    add  edit controller

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,8 +9,10 @@ namespace TestWebApp1.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+
         [DisplayName("Display Order")]
-        
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
